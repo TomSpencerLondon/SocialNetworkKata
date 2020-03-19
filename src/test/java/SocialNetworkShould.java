e53ff7ae195cdf5f0d0fc77d196cbeaa5a1e076c
Morinslash <sebastian.elf.kulinski@gmail.com>
2020-03-19 12:40:27 +0100

    initial commit and setup

---
+++ src/test/java/SocialNetworkShould.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class SocialNetworkShould {
+    @Test
+
+    public void dummy() {
+        assertTrue(true);
+    }
+}
