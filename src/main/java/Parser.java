f74cd259b04e6e8146596ca0aec0e66f95875e6f
Tom Spencer <tom@homeflow.co.uk>
2020-03-19 15:12:55 +0000

    Add return_username_action_and_argument test for Parser to return parsed array

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,5 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Parser {
+
+  public static final String REGEX = "(\\w+)(\\s(->)(\\s(.*))*)*";
+  private Pattern pattern;
+
+  public Parser() {
+    pattern = Pattern.compile(REGEX);
+  }
+
   public String[] parse(String input){
-    throw new UnsupportedOperationException();
+    Matcher matcher = pattern.matcher(input);
+    matcher.matches();
+    return new String[] {matcher.group(1), matcher.group(3), matcher.group(5)};
   }
 }
---
+++ src/test/java/ParserShould.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class ParserShould {
+
+  @Test
+  void return_username_action_and_argument() {
+    Parser parser = new Parser();
+    String[] expectedArray = {"Alice", "->", "I love the weather today"};
+    String input = "Alice -> I love the weather today";
+    assertArrayEquals(expectedArray, parser.parse(input));
+  }
+}
